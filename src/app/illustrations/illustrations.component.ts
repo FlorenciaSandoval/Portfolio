fe275811af69be2a0d9de26e61907dfb7e470cab
flor <florenciamacarenasandoval@gmail.com>
2024-04-26 12:04:59 +0200

    change illustrator expand img

--- src/app/illustrations/illustrations.component.ts
+++ src/app/illustrations/illustrations.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class IllustrationsComponent {
 
+  isActive: boolean = false;
+  isActive1:boolean=false; 
+  isActive2:boolean=false; 
+
+  popup(){
+    this.isActive = !this.isActive;
+   
+ 
+  }
+
+  popup1(){
+    this.isActive1 = !this.isActive1;
+   
+ 
+  }
+
+  popup2(){
+    this.isActive2 = !this.isActive2;
+   
+ 
+  }
+
 }
